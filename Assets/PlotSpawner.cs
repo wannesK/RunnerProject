ebcfc64c9ec04e07b06cd00a1c68525537e72f29
wannesK <66689660+wannesK@users.noreply.github.com>
2021-03-05 14:06:07 +0300

    Building spawner being done

---
+++ Assets/PlotSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlotSpawner : MonoBehaviour
+{
+    private int initAmount = 3;
+    private float plotSize = 64.5f;
+    private float xPosLeft = -2.9f;
+    private float xPosRight = 2.9f;
+    private float lastZPos = 0f;
+
+    public List<GameObject> leftPlots;
+    public List<GameObject> rightPlots;    
+    void Start()
+    {
+        for (int i = 0; i < initAmount; i++)
+        {
+            SpawnPlot();
+        }
+    }
+    public void SpawnPlot()
+    {
+        GameObject plotLeft = leftPlots[Random.Range(0, leftPlots.Count)];
+        GameObject plotRight = rightPlots[Random.Range(0, rightPlots.Count)];
+
+        float zPos = lastZPos + plotSize;
+        Instantiate(plotLeft, new Vector3(xPosLeft, 0.025f, zPos), plotLeft.transform.rotation);
+        Instantiate(plotRight, new Vector3(xPosRight, 0.025f, zPos), new Quaternion(0, 180, 0, 0));
+
+        lastZPos += plotSize;
+    }
+}
--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -3,12 +3,15 @@
 public class SpawnManager : MonoBehaviour
 {
     RoadSpawner roadSpawner;
+    PlotSpawner plotSpawner;
     void Start()
     {
         roadSpawner = GetComponent<RoadSpawner>();
+        plotSpawner = GetComponent<PlotSpawner>();
     }
     public void SpawnTriggerEntered()
     {
         roadSpawner.MoveRoad();
+        plotSpawner.SpawnPlot();
     }
 }
